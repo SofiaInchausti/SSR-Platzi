a667f1eda77b4ec418dec417ee21dd97f4ca8547
sofiainchausti <sofiabeleninchausti@gmail.com>
2024-05-27 13:54:43 -0300

    Fetching API from server side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,28 @@
 import { config } from "./config";
 import express, { Express, Request, Response } from "express";
 import { render } from "./render";
+import axios from "axios";
 
 const app: Express = express();
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.get('*',(req: Request, res: Response)=>{
-    res.send(render(req.url))
-})
+app.get("/galaxias", async (req: Request, resp: Response) => {
+  try {
+    const { data } = await axios.get(
+      "https://images-api.nasa.gov/search?=q=galaxies"
+    );
+    const initialProps = { galaxies: data?.collection?.items };
+    resp.send(render(req.url, initialProps));
+  } catch (error) {
+    throw new Error("An error ocurred in /galaxias", error);
+  }
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.send(render(req.url));
+});
 
 app.listen(config.PORT, () => {
-    console.log(`listening in ${config.PORT}`)
-})
+  console.log(`listening in ${config.PORT}`);
+});
