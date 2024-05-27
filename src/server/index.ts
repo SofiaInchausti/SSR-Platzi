7c725156c1313d62e94bbbd4fb92a1c5dbc83fe3
sofiainchausti <sofiabeleninchausti@gmail.com>
2024-05-27 12:21:25 -0300

    Rendering React from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,12 @@
 import { config } from "./config";
 import express, { Express, Request, Response } from "express";
 import { template } from "./render/template";
+import { render } from "./render";
 
 const app: Express = express();
 
 app.get('*',(req: Request, res: Response)=>{
-    res.send(template("<h1>Joaquin</h1>"))
+    res.send(render(req.url))
 })
 
 app.listen(config.PORT, () => {
